2a120253d582f10284d033e1a8b308ecc23d3a54
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-04-09 12:27:45 +0200

    add package level doc

---
+++ R/rversions-package.R
@@ -0,0 +1,10 @@
+#' @details The R version numbers and dates are extracted from the
+#' main R SVN repository at <http://svn.r-project.org/R/>.
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
