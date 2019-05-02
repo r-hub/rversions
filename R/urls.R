d21a71ad3f2ddb79dad25c8aa56ed48d4367c6d9
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-05-02 13:02:19 +0200

    make everything a function

--- R/urls.R
+++ R/urls.R
@@ -1,12 +1,23 @@
-
-r_svn_url <- "https://svn.r-project.org/R/tags/"
-
-if (nzchar(Sys.getenv("R_RVERSIONS_CRAN_URL"))){
-  cran_url <- Sys.getenv("R_RVERSIONS_CRAN_URL")
-} else {
-  cran_url <- "cran.r-project.org"
+r_svn_url <- function(){
+  "https://svn.r-project.org/R/tags/"
 }
 
-r_download_url <- paste0(cran_url, "/src/base/R-3/")
-r_win_download_url <- paste0(cran_url, "/bin/windows/base/")
-r_macos_download_url <- paste0(cran_url, "/bin/macosx/")
+cran_url <- function(){
+  if (nzchar(Sys.getenv("R_RVERSIONS_CRAN_URL"))){
+    Sys.getenv("R_RVERSIONS_CRAN_URL")
+  } else {
+    "cran.r-project.org"
+  }
+}
+
+r_download_url <- function(){
+  paste0(cran_url(), "/src/base/R-3/")
+}
+
+r_win_download_url <- function(){
+  paste0(cran_url(), "/bin/windows/base/")
+}
+
+r_macos_download_url <- function(){
+  paste0(cran_url(), "/bin/macosx/")
+}
