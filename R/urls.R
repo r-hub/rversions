8d855e002467077f1e8bd8e1e71333871f1cafb2
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-05-02 12:54:30 +0200

    add env variable to change CRAN url

--- R/urls.R
+++ R/urls.R
@@ -1,5 +1,12 @@
 
 r_svn_url <- "https://svn.r-project.org/R/tags/"
-r_download_url <- "https://cran.r-project.org/src/base/R-3/"
-r_win_download_url <- "https://cran.r-project.org/bin/windows/base/"
-r_macos_download_url <- "https://cran.r-project.org/bin/macosx/"
+
+if (nzchar(Sys.getenv("R_RVERSIONS_CRAN_URL"))){
+  cran_url <- Sys.getenv("R_RVERSIONS_CRAN_URL")
+} else {
+  cran_url <- "cran.r-project.org"
+}
+
+r_download_url <- paste0(cran_url, "/src/base/R-3/")
+r_win_download_url <- paste0(cran_url, "/bin/windows/base/")
+r_macos_download_url <- paste0(cran_url, "/bin/macosx/")
